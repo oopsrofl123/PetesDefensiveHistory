5dad2c17805cc206fa3e3a0393197d28d885a619
oopsrofl123 <oopsrofl123@github.com>
2026-02-14 11:40:11 -0500

    Reduce the number of tracked successful casts

--- Globals.lua
+++ Globals.lua
@@ -6,7 +6,7 @@ local _, ns = ...
 
 ns.INFINITY = 10*24*3600  -- A much longer time than any reasonble value (10 days)
 ns.MAX_HISTORY = 4
-ns.MAX_CAST_HISTORY = 60  -- buffs can last a long time. a minute worth of casts for a 1s GCD character
+ns.MAX_CAST_HISTORY = 8  -- must be cycled through on every unit on every cast succeeded. keep low.
 ns.ICON_SIZE = 32
 ns.ICON_SPACING = 2
 ns.DEFAULT_ICON = 134400 -- question mark
