48809aaac3e16f8e7accdb6844491baa5bf978b8
oopsrofl123 <oopsrofl123@github.com>
2026-02-15 08:49:07 -0500

    Major cleanup of UI allocation, layout and updating

--- SpecHandlerUI.lua
+++ SpecHandlerUI.lua
@@ -22,86 +22,55 @@ local UI_HEIGHT = 10 + UI_TITLE_PADDING + 20 + UI_CLOSE_BUTTON_HEIGHT + 20 + UI_
 
 
 
-function ns:showPdhGroupSolutionRow(row)
-    local index = ns:slotToIndex(row.slot)
-
-    row:SetSize(UI_WIDTH, UI_ROW_HEIGHT)
-    row:SetPoint("TOP", ns.groupSolutionUIFrame.uiTitle, "TOP", 0, -UI_ROW_HEIGHT * (index - 1) - UI_TITLE_PADDING)
-
-    -- Anchor the row from right to left so that the grid of icons is aligned
-    for i=UI_MAX_ABILITIES_PER_SLOT,1,-1 do
-        local icon = row.abilityIcons[i]
-        icon:SetSize(UI_ICON_SIZE, UI_ICON_SIZE)
-        if i == UI_MAX_ABILITIES_PER_SLOT then
-            icon:SetPoint("RIGHT", row, "RIGHT", -UI_ICON_PADDING, 0)
-        else
-            icon:SetPoint("RIGHT", row.abilityIcons[i+1], "LEFT", -UI_ICON_PADDING, 0)
-        end
-        icon:SetTexture("Interface\\Icons\\Spell_Nature_HealingTouch")
-        icon:Show()
-
-        local cooldownLabel = row.abilityCooldownLabel[i]
-        cooldownLabel:SetPoint("CENTER", icon, "CENTER")
-        local durationLabel = row.abilityDurationLabel[i]
-        durationLabel:SetPoint("TOP", icon, "BOTTOM", 0, -3)
-        local conflictsLabel = row.abilityConflictsLabel[i]
-        conflictsLabel:SetPoint("TOP", durationLabel, 'BOTTOM', 0, -3)
-    end
-
+function ns:updateGroupSolutionRow(row)
     -- Fill in icons from left to right..obviously
     for i=1,UI_MAX_ABILITIES_PER_SLOT do
-        local icon = row.abilityIcons[i]
+        local item = row.items[i]
         local ability = ns.groupCDs[row.slot].abilities[i]
-        local conflictsLabel = row.abilityConflictsLabel[i]
-        local cooldownLabel = row.abilityCooldownLabel[i]
-        local durationLabel = row.abilityDurationLabel[i]
+
         if ability then
             if ability.iconId then
                 -- Override the standard spell icon
-                icon:SetTexture(ability.iconId)
+                item.icon:SetTexture(ability.iconId)
             else
-                icon:SetTexture(C_Spell.GetSpellInfo(ability.id).iconID)
+                item.icon:SetTexture(C_Spell.GetSpellInfo(ability.id).iconID)
             end
-            cooldownLabel:SetText(ability.cooldown .. "s")
-            durationLabel:SetText(ability.duration .. "s")
             if not ability.solved then
-                icon:SetDesaturated(true)
+                item.icon:SetDesaturated(true)
             end
-            -- if ability.conflicts and #ability.conflicts > 0 then
+            item.icon:Show()
+
+            item.cooldownLabel:SetText(ability.cooldown .. "s")
+            item.cooldownLabel:Show()
+
+            item.durationLabel:SetText(ability.duration .. "s")
+            item.durationLabel:Show()
+
             -- XXX: TODO: line below bugs on offline players
             if #ability.conflicts > 0 then
-                conflictsLabel:SetText(table.concat(ability.conflicts, ' '))
-                conflictsLabel:SetTextColor(1,0,0)
-                conflictsLabel:Show()
+                item.conflictsLabel:SetText(table.concat(ability.conflicts, ' '))
+                item.conflictsLabel:SetTextColor(1,0,0)
+                item.conflictsLabel:Show()
             end
-            icon:Show()
         else
-            conflictsLabel:Hide()
-            cooldownLabel:Hide()
-            durationLabel:Hide()
-            icon:Hide()
+            item.icon:Hide()
+            item.conflictsLabel:Hide()
+            item.cooldownLabel:Hide()
+            item.durationLabel:Hide()
         end
     end
 
-    row.nameLabel:SetPoint("TOPRIGHT", row.abilityIcons[1], "TOPLEFT", -UI_ICON_PADDING, -5)
-    row.nameLabel:SetText(UnitName(row.slot))
-    row.nameLabel:Show()
-
-    row.specLabel:SetPoint("BOTTOMRIGHT", row.abilityIcons[1], "BOTTOMLEFT", -UI_ICON_PADDING, 5)
-    if ns.historyRows[row.slot] then
-        if ns.groupCDs[row.slot].specId then
-            row.specLabel:SetText(ns:specIdToString(ns.groupCDs[row.slot].specId))
-            _, class = UnitClass(row.slot)
-            classColor = RAID_CLASS_COLORS[class]
-            row.specLabel:SetTextColor(classColor.r, classColor.g, classColor.b)
-        end
+    -- Update and color class/specialization label if it was detected
+    if ns.groupCDs[row.slot].specId then
+        row.specLabel:SetText(ns:specIdToString(ns.groupCDs[row.slot].specId))
+        _, class = UnitClass(row.slot)
+        classColor = RAID_CLASS_COLORS[class]
+        row.specLabel:SetTextColor(classColor.r, classColor.g, classColor.b)
+        row.specLabel:Show()
     end
-    row.specLabel:Show()
 
 
     -- Determine the role of the party member and set the appropriate icon
-    row.roleIcon:SetSize(20, 20)
-    row.roleIcon:SetPoint("LEFT", row, "LEFT", 5+10, 0)  -- inset size matters
     row.roleIcon:Show()
     local role = UnitGroupRolesAssigned(row.slot)
     if role == "TANK" then
@@ -111,51 +80,90 @@ function ns:showPdhGroupSolutionRow(row)
     elseif role == "DAMAGER" then
         row.roleIcon:SetTexCoord(0.3125, 0.609375, 0.34375, 0.640625)
     else
-        ns:printDebug("unrecognized role " .. role .. " for slot " .. row.slot)
+        -- not unusual. happens when not in group
+        row.roleIcon:Hide()
     end
-
 end
 
 
 
-local function allocPdhGroupSolutionRow(slot, index)
+local function allocSolutionItem(row)
+    local item = {}
+    item.icon = row:CreateTexture(nil, "OVERLAY")
+    item.icon:SetSize(UI_ICON_SIZE, UI_ICON_SIZE)
+    item.icon:SetTexture("Interface\\Icons\\Spell_Nature_HealingTouch")
+    item.icon:Hide()
+
+    item.cooldownLabel = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormalLarge')
+    font, height, flags = item.cooldownLabel:GetFont()
+    item.cooldownLabel:SetFont(font, height, "THICKOUTLINE")
+    item.cooldownLabel:SetTextColor(1,1,1)
+    item.cooldownLabel:SetPoint("CENTER", item.icon, "CENTER")
+    item.cooldownLabel:Hide()
+
+    item.durationLabel = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+    item.durationLabel:SetTextColor(1,1,1)
+    item.durationLabel:SetPoint("TOP", item.icon, "BOTTOM", 0, -3)
+    item.durationLabel:Hide()
+
+    item.conflictsLabel = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
+    item.conflictsLabel:SetPoint("TOP", item.durationLabel, 'BOTTOM', 0, -3)
+    item.conflictsLabel:Hide()
+
+    return item
+end
+
+
+
+local function allocGroupSolutionRow(uiframe, slot, index)
     -- Create the row frame
-    local row = CreateFrame("Frame", nil, ns.groupSolutionUIFrame)
+    local row = CreateFrame("Frame", "Row_"..slot, uiframe)
+    row:SetSize(UI_WIDTH, UI_ROW_HEIGHT)
+    row:SetPoint("TOP", uiframe.uiTitle, "TOP", 0, -UI_ROW_HEIGHT * (index - 1) - UI_TITLE_PADDING)
 
     row.slot = slot
+
+    -- Player names
     row.nameLabel = row:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+    row.nameLabel:SetText(UnitName(slot))
+    row.nameLabel:Show()
+
+    -- Player class and specialization
     row.specLabel = row:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    row.specLabel:SetText("")
+    row.specLabel:Hide() -- only show when spec is detected
+
+    -- tank, healer, dps icon
     row.roleIcon = row:CreateTexture(nil, "OVERLAY")
     row.roleIcon:SetTexture('Interface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES')
-    row.abilityIcons = {}
-    row.abilityConflictsLabel = {}
-    row.abilityCooldownLabel = {}
-    row.abilityDurationLabel = {}
+    row.roleIcon:SetSize(20, 20)
+    row.roleIcon:SetPoint("LEFT", row, "LEFT", 5+10, 0)  -- inset size matters
+    row.specLabel:Hide() -- only show when a role is detected
 
+    row.items = {}
     for i=1,UI_MAX_ABILITIES_PER_SLOT do
-        row.abilityIcons[i] = row:CreateTexture(nil, "OVERLAY")
-        row.abilityConflictsLabel[i] = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
-
-        row.abilityCooldownLabel[i] = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormalLarge')
-        font, height, flags = row.abilityCooldownLabel[i]:GetFont()
-        row.abilityCooldownLabel[i]:SetFont(font, height, "THICKOUTLINE")
-        row.abilityCooldownLabel[i]:SetTextColor(1,1,1)
-
-        row.abilityDurationLabel[i] = row:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
-        row.abilityDurationLabel[i]:SetTextColor(1,1,1)
+        row.items[i] = allocSolutionItem(row)
     end
 
-    ns.groupSolutionUI[slot] = row
+    -- Anchor the row from right to left so that the grid of icons is left-aligned
+    row.items[UI_MAX_ABILITIES_PER_SLOT].icon:SetPoint("RIGHT", row, "RIGHT", -UI_ICON_PADDING, 0)
+    for i=UI_MAX_ABILITIES_PER_SLOT-1,1,-1 do
+        row.items[i].icon:SetPoint("RIGHT", row.items[i+1].icon, "LEFT", -UI_ICON_PADDING, 0)
+    end
+
+    row.nameLabel:SetPoint("TOPRIGHT", row.items[1].icon, "TOPLEFT", -UI_ICON_PADDING, -5)
+    row.specLabel:SetPoint("BOTTOMRIGHT", row.items[1].icon, "BOTTOMLEFT", -UI_ICON_PADDING, 5)
 
     return row
 end
 
 
 
-function ns:showPdhGroupSolutionUI()
-    ns.groupSolutionUIFrame:SetSize(UI_WIDTH, UI_HEIGHT)
-    ns.groupSolutionUIFrame:SetPoint("CENTER")
-    ns.groupSolutionUIFrame:SetBackdrop({
+local function allocMainUIFrame()
+    local f = CreateFrame("Frame", addonName .. "GroupSolutionUI", UIParent, "BackdropTemplate")
+    f:SetSize(UI_WIDTH, UI_HEIGHT)
+    f:SetPoint("CENTER")
+    f:SetBackdrop({
         bgFile = "Interface\\TutorialFrame\\TutorialFrameBackground",
         edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
         tile = true,
@@ -165,13 +173,25 @@ function ns:showPdhGroupSolutionUI()
         insets = {left = 3, right = 5, top = 3, bottom = 5}
     })
 
-    ns.groupSolutionUIFrame:SetMovable(true)
-    ns.groupSolutionUIFrame:EnableMouse(true)
-    ns.groupSolutionUIFrame:RegisterForDrag("LeftButton")
-    ns.groupSolutionUIFrame:SetScript("OnDragStart", function(self, button) self:StartMoving() end)
-    ns.groupSolutionUIFrame:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
+    f:SetMovable(true)
+    f:EnableMouse(true)
+    f:RegisterForDrag("LeftButton")
+    f:SetScript("OnDragStart", function(self, button) self:StartMoving() end)
+    f:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
 
-    ns.groupSolutionUIFrame:Show()
+    f.uiTitle = f:CreateFontString(nil, 'OVERLAY', 'GameFontNormalLarge')
+    f.uiTitle:SetText("PetesDefensiveHistory")
+    f.uiTitle:SetPoint("TOP", f, "TOP", 0, -10)
+
+    local closeButton = CreateFrame("Button", "closeButton", f, 'UIPanelButtonTemplate')
+    closeButton:SetSize(UI_WIDTH/2, UI_CLOSE_BUTTON_HEIGHT)
+    closeButton:SetPoint("BOTTOM", f, "BOTTOM", 0, 15)
+    closeButton:SetText("Close")
+    -- not sure why variable scoping binds f in the function call below to this local f, but whatever
+    closeButton:SetScript("OnClick", function() f:Hide() end)
+
+    f:Show()
+    return f
 end
 
 
@@ -179,22 +199,13 @@ end
 -- IMPORTANT: do not make assumptions about whether group ability solution code
 -- has been run when allocating the solution UI. The solutions happen at uncontrollable
 -- times during LibSpec callbacks.
-function ns:allocPdhGroupSolutionUI()
-    ns.groupSolutionUIFrame = CreateFrame("Frame", addonName .. "GroupSolutionUI", UIParent, "BackdropTemplate")
-    ns:showPdhGroupSolutionUI()
+function ns:allocGroupSolutionUI()
+    local solutionUI = allocMainUIFrame()
 
-    ns.groupSolutionUIFrame.uiTitle = ns.groupSolutionUIFrame:CreateFontString(nil, 'OVERLAY', 'GameFontNormalLarge')
-    ns.groupSolutionUIFrame.uiTitle:SetText("PetesDefensiveHistory")
-    ns.groupSolutionUIFrame.uiTitle:SetPoint("TOP", ns.groupSolutionUIFrame, "TOP", 0, -10)
-
+    solutionUI.rows = {}
     for slot, index in pairs(ns.allSlots) do
-        row = allocPdhGroupSolutionRow(slot, index)
-        ns:showPdhGroupSolutionRow(row)
+        solutionUI.rows[slot] = allocGroupSolutionRow(solutionUI, slot, index)
     end
 
-    local closeButton = CreateFrame("Button", nil, ns.groupSolutionUIFrame, 'UIPanelButtonTemplate')
-    closeButton:SetSize(UI_WIDTH/2, UI_CLOSE_BUTTON_HEIGHT)
-    closeButton:SetPoint("BOTTOM", ns.groupSolutionUIFrame, "BOTTOM", 0, 15)
-    closeButton:SetText("Close")
-    closeButton:SetScript("OnClick", function() ns.groupSolutionUIFrame:Hide() end)
+    return solutionUI
 end
