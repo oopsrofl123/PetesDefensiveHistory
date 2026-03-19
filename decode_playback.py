7c3afcee966cbfaf78e3b2ce31e87e894c7216aa
oopsrofl123 <oopsrofl123@github.com>
2026-03-19 09:59:08 -0400

    First work to print inference strings mimicking in-game logging

--- decode_playback.py
+++ decode_playback.py
@@ -7,13 +7,72 @@ import zlib
 import cbor2
 import numpy as np
 
+ascii_cyan = "\033[36m"
+ascii_green = "\033[32m"
+ascii_red = "\033[31m"
+ascii_reset = "\033[0m"
+
+def passcolor(passes):
+    return ascii_green if passes else ascii_red
+
+
+def certainmark(certain):
+    return "" if certain else "*"
+
+
 def decode_export_blob(compressed):
     decoded = base64.b64decode(compressed)
     decompressed = zlib.decompress(decoded, wbits=-15)
     return cbor2.loads(decompressed)
+
+
+def one_logic_string(ability_id, layers):
+    string = "[" + str(ability_id) + ":"
+    for name, summary in layers.items():
+        passes, certain = summary
+        string += "%s%s%s" % (passcolor(passes), name.decode(), certainmark(certain))
+    string += ascii_reset + "]"
+    return string
+
+
+def logic_string(logic, pass_type):
+    string = ""
+    for logic_summary in logic:
+        caster, ability_id, passes, layers = logic_summary
+        if passes == pass_type:
+            string += one_logic_string(ability_id, layers)
+    return string
+
+
+def confidence_string(conf):
+    ability_id, certain, num_possible_solns, layers = conf
+    string = "#S=%d match=[%s], layers=[" % \
+        (num_possible_solns, "nil" if ability_id is None else ability_id)
+    for name, summary in layers.items():
+        string += passcolor(summary[0]) + name.decode() + certainmark(summary[1]) + ascii_reset
+    string += "]"
+    return string
+
+
+def req_string(reqs):
+    string = ""
+    for req in reqs:
+        passcolor(reqs)
+
+
+def infer_string(inf):
+    inference_time, inference_trace, inference_attempt, \
+        event_time, event_trace, event_id, event_source, event_slot, batch_id, \
+        ability_id, certain, ability_caster, \
+        logic, conf, reqs = inf
+    return "%0.3f %sInfer(tr(infer)=[%s], tr(event)=[%s], source=[%s], eventId=[%s/%d], attempt=[%d]%s" % \
+        (inference_time, ascii_cyan, inference_trace, event_trace, event_slot, event_id, batch_id, inference_attempt, ascii_reset)
     
+
+
 with open("inference_and_playback_data.txt", "r") as f:
     data = decode_export_blob(f.read())
+    print(data.keys())
     metadata = data[b'metadata']
     metadata = { k.decode(): (v.decode() if type(v) is bytes else v) for k, v in metadata.items() }
     print('METADATA ------------------------------------------------------------------------')
@@ -21,12 +80,23 @@ with open("inference_and_playback_data.txt", "r") as f:
     print('Exporter:', player_guid)
 
 
-    characters = data[b'characters']
+    character_updates = data[b'characterUpdates']
     print('CHARACTERS ----------------------------------------------------------------------')
-    print('got', len(characters), 'characters')
-    for char in characters:
-        char = { k.decode(): (v.decode() if type(v) is bytes else v) for k, v in char.items() }
-        print(char['playerName'], char['GUID'], char['raceName'], char['specName'], char['classFile'])
+    print('got', len(character_updates), 'character updates')
+    for update in character_updates:
+        time, trace, characters = update
+        print('    time=[%0.3f] responded to [%s]: character data:' % (time, trace.decode()))
+        for char in characters:
+            char = { k.decode(): (v.decode() if type(v) is bytes else v) for k, v in char.items() }
+            print('        ', end='')
+            for k, v in char.items():
+                thisv = str(v)
+                if type(v) is list:
+                    thisv = '[%d]' % len(v)
+                if type(v) is dict:
+                    thisv = '{%d}' % len(v)
+                print(" " + k + "=" + thisv, end='')
+            print('\n', end='')
 
     playback = data[b'playback']
     print('PLAYBACK ------------------------------------------------------------------------')
@@ -54,10 +124,16 @@ with open("inference_and_playback_data.txt", "r") as f:
 
     # for each inference, find the closest matching cast record
     for inf in inferences:
+        inf = [ x.decode() if type(x) is bytes else x for x in inf ]
         inference_time, inference_trace, inference_attempt, \
         event_time, event_trace, event_id, event_source, event_slot, batch_id, \
         ability_id, certain, ability_caster, \
         logic, conf, reqs = inf
         best = np.argmin(np.absolute(cast_times - inference_time))
         #print(inference_time, best, ability_id, cast_events[best])
-        print(inf)
+        if ability_id == 389539: #True or event_source == b"Player-3721-0C5111C6":
+            print(infer_string(inf))
+            print("PASS:", logic_string(logic, True))
+            print("FAIL:", logic_string(logic, False))
+            print("CONF:", confidence_string(conf))
+            #print(inf)
